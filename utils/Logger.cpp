d8fe8208c467b25c14c32d3da339a8f539057f0d
Sergey Vaytsel <seralwei@gmail.com>
2015-10-17 10:47:42 +0600

    #12 Add 1st logger implementation

--- utils/Logger.cpp
+++ utils/Logger.cpp
@@ -1,7 +1,79 @@
 #include "Logger.h"
 
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <chrono>
+#include <ctime>
 
-Logger::Logger ( ) { }
+using std::chrono::system_clock;
+using std::time_t;
+using std::ctime;
+using std::clog;
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::ofstream;
+using std::vector;
+using std::string;
+using std::pair;
+using std::for_each;
+
+utils::Logger::Logger(std::string className, LoggerConfig config) : _className(className) {
+  for (auto p : config.files) this->_files[p.first].push_back(p.second);  
+  this->_logLevel = config.logLevel;
+  this->_showDate = config.showDate;
+}
+
+utils::Logger::~Logger() { }
 
 
-Logger::~Logger ( ) { }
+bool utils::Logger::_accept(LEVEL lvl) {
+  return static_cast<int>(this->_logLevel) <= static_cast<int>(lvl);
+}
+
+string utils::Logger::_date() {
+  if (this->_showDate) {
+    auto _ = system_clock::to_time_t(system_clock::now());
+    const size_t size = 100;
+    char buf[size];
+    ctime_s(buf, size, &_);
+    return string(buf);
+  }
+  else return string();
+}
+
+void utils::Logger::debug(std::string message) {
+  static vector<string> files = this->_files[LEVEL::DEBUG];
+  if (_accept(LEVEL::DEBUG)) {
+    clog << _date() << " [DEBUG] " << this->_className << " " << message << endl;
+  }
+}
+
+void utils::Logger::info(std::string message)  {
+  static vector<string> files = this->_files[LEVEL::INFO];
+  if (_accept(LEVEL::INFO)) {
+    cout << _date() << " [INFO] " << this->_className << " " << message << endl;
+  }
+}
+
+void utils::Logger::warn(std::string message)  {
+  static vector<string> files = this->_files[LEVEL::WARN];
+  if (_accept(LEVEL::WARN)) {
+    clog << _date() << " [WARN] " << this->_className << " " << message << endl;
+  }
+}
+
+void utils::Logger::error(std::string message) {
+  static vector<string> files = this->_files[LEVEL::ERROR];
+  if (_accept(LEVEL::ERROR)) {
+    cerr << _date() << " [ERROR] " << this->_className << " " << message << endl;
+  }
+}
+
+void utils::Logger::fatal(std::string message) {
+  static vector<string> files = this->_files[LEVEL::FATAL];
+  if (_accept(LEVEL::FATAL)) {
+    cerr << _date() << " [FATAL] " << this->_className << " " << message << endl;
+  }
+}
--- utils/Logger.h
+++ utils/Logger.h
@@ -1,10 +1,64 @@
 #ifndef LOGGER_H_
 #define LOGGER_H_
 
-class Logger {
-public:
-  Logger ( );
-  ~Logger ( );
-};
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+
+namespace utils {
+  const enum LEVEL {
+    DEBUG = 0,
+    WARN,
+    INFO,
+    ERROR,
+    FATAL
+  };
+
+  using logfilemap = std::vector<std::pair<LEVEL, std::string>>;
+  using _logfilemap = std::map<LEVEL, std::vector<std::string>>;
+
+  const logfilemap logFiles = {
+    { LEVEL::DEBUG, "../logs/Ewe.log" }
+  };
+
+  class LoggerConfig {
+  public:
+    logfilemap files;
+    LEVEL logLevel;
+    bool showDate;
+  };
+
+  class DefaultLoggerConfig : public LoggerConfig {
+  public:
+    DefaultLoggerConfig() {
+      files = logFiles;
+      logLevel = LEVEL::WARN;
+      showDate = true;
+    }
+  };
+
+  class Logger {
+  private:
+    LEVEL _logLevel;
+    _logfilemap _files;
+    bool _showDate;
+
+    std::string _className;
+
+    bool _accept(LEVEL lvl);
+    std::string _date();
+  public:
+    Logger(std::string className, LoggerConfig config = DefaultLoggerConfig());
+    ~Logger();
+
+    void debug(std::string message);
+    void warn(std::string message);
+    void info(std::string message);
+    void error(std::string message);
+    void fatal(std::string message);
+  };
+
+}
 
 #endif // LOGGER_H_
