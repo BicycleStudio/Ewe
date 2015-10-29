a3644755f6785a90971a2cd5aef33239db6e030f
Sergey Vaytsel <seralwei@gmail.com>
2015-10-29 21:46:59 +0600

    ThreadManager.cpp refactoring

--- thread_manager/ThreadManager.cpp
+++ thread_manager/ThreadManager.cpp
@@ -2,97 +2,113 @@
 
 static const int threadManagerSleep = 100;
 
-thread_manager::ThreadSubject::ThreadSubject() { 
-  this->commands_ = std::make_shared<std::queue<command_manager::Command>>();
-  this->_willStop = false;
-  this->_paused = false;
-}
+using std::shared_ptr;
+using std::make_shared;
+using std::queue;
+using std::thread;
+using std::this_thread::sleep_for;
+using std::chrono::milliseconds;
 
-void thread_manager::ThreadSubject::processCommands ( ) {
-  while (this->commands_->size () > 0) {
-    auto& c = this->commands_->front();
-    processCommand (c);
-    this->commands_->pop();
+using command_manager::CommandManager;
+using command_manager::Command;
+using command_manager::CommandType;
+using command_manager::ID;
+
+namespace thread_manager {
+
+  ThreadSubject::ThreadSubject() {
+    this->commands_ = make_shared<queue<Command>>();
+    this->_willStop = false;
+    this->_paused = false;
   }
-}
 
-void thread_manager::ThreadSubject::bind(command_manager::CommandManager* commandManager) {
-  commandManager_ = commandManager;
-}
-
-std::shared_ptr<std::queue<command_manager::Command>> thread_manager::ThreadSubject::getQueueLink ( ) {
-  return this->commands_;
-}
-
-void thread_manager::ThreadSubjectWithKill::_sendKill() {
-  command_manager::Command commandKill = command_manager::Command(
-    this->id(), command_manager::ID::THREAD_MANAGER,
-    command_manager::CommandType::KILL);
-  commandManager_->push(commandKill);
-}
-
-thread_manager::ThreadManager::ThreadManager ( ) {
-  this->commands_ = std::make_shared<std::queue<command_manager::Command>>();
-  this->commandManager_.addQueue (
-    command_manager::ID::THREAD_MANAGER,
-    this->commands_
-  );
-}
-
-void thread_manager::ThreadManager::add(ThreadSubject * tc) {
-  tc->bind(&commandManager_);
-  
-  this->subjects_.push_back (tc);
-  this->commandManager_.addQueue (tc->id ( ), tc->getQueueLink ( ));
-}
-
-void thread_manager::ThreadManager::start ( ) {
-  for (auto c : subjects_)
-    threads_.push_back(std::thread([&c] () { c->start(); }));
-}
-
-void thread_manager::ThreadManager::stop() {
-  for (auto& c : subjects_) c->stop ();
-  for (auto& t : threads_) t.join();
-
-  subjects_.clear ();
-  threads_.clear();
-}
-
-void thread_manager::ThreadManager::pause() {
-  for (auto& c : subjects_) c->pause();
-}
-
-void thread_manager::ThreadManager::resume() {
-  for (auto& c : subjects_) c->resume();
-}
-
-void thread_manager::ThreadManager::listen ( ) {
-  while (true) {
-    auto a = std::chrono::milliseconds(threadManagerSleep);
-    std::this_thread::sleep_for (a);
-
-    commandManager_.process ();
-
-    while (this->commands_->size () > 0) {
+  void ThreadSubject::processCommands() {
+    while (this->commands_->size() > 0) {
       auto& c = this->commands_->front();
-      switch (c.commandType) {
+      processCommand(c);
+      this->commands_->pop();
+    }
+  }
 
-        case command_manager::CommandType::KILL: 
-          this->stop ( );
+  void ThreadSubject::bind(CommandManager* commandManager) {
+    commandManager_ = commandManager;
+  }
+
+  shared_ptr<queue<Command>> ThreadSubject::getQueueLink() {
+    return this->commands_;
+  }
+
+  void ThreadSubjectWithKill::_sendKill() {
+    Command commandKill = Command(
+      this->id(), ID::THREAD_MANAGER,
+      CommandType::KILL);
+    commandManager_->push(commandKill);
+  }
+
+  ThreadManager::ThreadManager() {
+    this->commands_ = make_shared<queue<Command>>();
+    this->commandManager_.addQueue(
+      ID::THREAD_MANAGER,
+      this->commands_
+      );
+  }
+
+  void ThreadManager::add(ThreadSubject * tc) {
+    tc->bind(&commandManager_);
+
+    this->subjects_.push_back(tc);
+    this->commandManager_.addQueue(tc->id(), tc->getQueueLink());
+  }
+
+  void ThreadManager::start() {
+    for (auto c : subjects_)
+      threads_.push_back(thread([&c]() { c->start(); }));
+  }
+
+  void ThreadManager::stop() {
+    for (auto& c : subjects_) c->stop();
+    for (auto& t : threads_) t.join();
+
+    subjects_.clear();
+    threads_.clear();
+  }
+
+  void ThreadManager::pause() {
+    for (auto& c : subjects_) c->pause();
+  }
+
+  void ThreadManager::resume() {
+    for (auto& c : subjects_) c->resume();
+  }
+
+  void ThreadManager::listen() {
+    while (true) {
+      auto a = milliseconds(threadManagerSleep);
+      sleep_for(a);
+
+      commandManager_.process();
+
+      while (this->commands_->size() > 0) {
+        auto& c = this->commands_->front();
+        switch (c.commandType) {
+
+        case CommandType::KILL:
+          this->stop();
           return;
-        
-        case command_manager::CommandType::PAUSE:
+
+        case CommandType::PAUSE:
           this->pause();
           break;
 
-        case command_manager::CommandType::RESUME:
+        case CommandType::RESUME:
           this->resume();
           break;
 
         default: break;
+        }
+        this->commands_->pop();
       }
-      this->commands_->pop();
     }
   }
+
 }
